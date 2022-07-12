0d03e0ac45931f213bc6fee3d9cd9d531f2d1908
andy <liuxiaohong9527@gmail.com>
2022-07-13 02:39:58 +0800

    更换下载地址

--- public/config.js
+++ public/config.js
@@ -1,11 +1,11 @@
 
 /*eslint-disable */
 /*测试环境*/
-// const _appDownloadUlr = 'http://img.wuhaicj.com/haibao.apk'
+// const _appDownloadUlr = 'http://zhuoyixiaochengxu.cn/haibao_100.apk'
 // const _requestApiUrl = 'http://api.wuhaicj.com'
 // const _requestWS = 'ws://ws.wuhaicj.com/'
 
 /*线上环境*/
-const _appDownloadUlr = 'http://cri1jba.cn/haibao.apk'
+const _appDownloadUlr = 'http://cri1jba.cn/haibao_100.apk'
 const _requestApiUrl = 'http://api.hljhwyy.cn'
 const _requestWS = 'http://api.hljhwyy.cn'
