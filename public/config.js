1d89e432acca7d252853f73d1b1c62b0113d3aaa
alvin <applewennie895@163.com>
2022-07-14 00:23:52 +0800

    线上地址修改

--- public/config.js
+++ public/config.js
@@ -1,11 +1,11 @@
 
 /*eslint-disable */
 /*测试环境*/
-const _appDownloadUlr = 'http://zhuoyixiaochengxu.cn/haibao_100.apk'
-const _requestApiUrl = 'http://api.wuhaicj.com'
-const _requestWS = 'ws://ws.wuhaicj.com/'
+// const _appDownloadUlr = 'http://zhuoyixiaochengxu.cn/haibao_100.apk'
+// const _requestApiUrl = 'http://api.wuhaicj.com'
+// const _requestWS = 'ws://ws.wuhaicj.com/'
 
 /*线上环境*/
-// const _appDownloadUlr = 'http://cri1jba.cn/haibao_100.apk'
-// const _requestApiUrl = 'http://api.hljhwyy.cn'
-// const _requestWS = 'http://api.hljhwyy.cn'
+const _appDownloadUlr = 'http://cri1jba.cn/haibao_100.apk'
+const _requestApiUrl = 'http://api.hljhwyy.cn'
+const _requestWS = 'http://api.hljhwyy.cn'
